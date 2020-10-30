08e622d972ad6a9a1934bd0fcbf3ab564fd18938
laughingsquirrel <keith@laughingsquirrel.net>
2020-10-30 15:15:45 -0400

    Update Dashboard.js

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,7 +1,5 @@
 import React, { lazy } from 'react'
 import {
-  CButton,
-  CButtonGroup,
   CCard,
   CCardBody,
   CCardFooter,
@@ -24,24 +22,6 @@ const Dashboard = () => {
               <h4 id="traffic" className="card-title mb-0">Traffic</h4>
               <div className="small text-muted">November 2017</div>
             </CCol>
-            <CCol sm="7" className="d-none d-md-block">
-              <CButton color="primary" className="float-right">
-                <CIcon name="cil-cloud-download"/>
-              </CButton>
-              <CButtonGroup className="float-right mr-3">
-                {
-                  ['Day', 'Month', 'Year'].map(value => (
-                    <CButton
-                      color="outline-secondary"
-                      key={value}
-                      className="mx-0"
-                      active={value === 'Month'}
-                    >
-                      {value}
-                    </CButton>
-                  ))
-                }
-              </CButtonGroup>
             </CCol>
           </CRow>
         </CCardBody>
