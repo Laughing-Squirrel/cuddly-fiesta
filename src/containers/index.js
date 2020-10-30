d77a63dc9fbd8102e58d07f979a8c1007d10c9be
laughingsquirrel <keith@laughingsquirrel.net>
2020-10-30 09:20:10 -0400

    Update index.js

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,9 +2,6 @@ import TheContent from './TheContent'
 import TheFooter from './TheFooter'
 import TheHeader from './TheHeader'
 import TheHeaderDropdown from './TheHeaderDropdown'
-//import TheHeaderDropdownMssg from './TheHeaderDropdownMssg'
-//import TheHeaderDropdownNotif from './TheHeaderDropdownNotif'
-//import TheHeaderDropdownTasks from './TheHeaderDropdownTasks'
 import TheLayout from './TheLayout'
 import TheSidebar from './TheSidebar'
 
@@ -17,7 +14,3 @@ export {
   TheSidebar
 }
 
-
-//{/*  TheHeaderDropdownMssg,
-//  TheHeaderDropdownNotif,
-//  TheHeaderDropdownTasks,*/}
