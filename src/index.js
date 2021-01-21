a1227786ca4a55508f589666454ab7b6c00427a8
laughingsquirrel <keith@laughingsquirrel.net>
2021-01-21 14:37:43 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import UserService from "./services/UserService";
 import { icons } from './assets/icons'
 
 import { Provider } from 'react-redux'
@@ -19,6 +19,8 @@ ReactDOM.render(
   </Provider>, 
   document.getElementById('root')
 );
+
+UserService.initKeycloak(renderApp);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
