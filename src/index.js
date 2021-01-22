274fcf6a39e399f9c2b785c4156255aaca1e0e1d
laughingsquirrel <keith@laughingsquirrel.net>
2021-01-22 14:34:29 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ React.icons = icons
 
 //UserService.initKeycloak(renderApp);
 
-ReactDOM.render(<App {...{ store, history }} />, document.getElementById('root'), UserService.initKeycloak);
+ReactDOM.render(<Provider store={store}> <App/> </Provider>, document.getElementById('root'), UserService.initKeycloak);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
