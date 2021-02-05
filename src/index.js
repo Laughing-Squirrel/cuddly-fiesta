42badf3dc7cdb33107aeb86b39d81c9f6fb0aadd
laughingsquirrel <keith@laughingsquirrel.net>
2021-02-05 09:49:04 -0500

    Rollback Security

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import HttpService from "./services/HttpService";
-import UserService from "./services/UserService";
 
 import { icons } from './assets/icons'
 
@@ -15,9 +13,12 @@ import store from './store'
 
 React.icons = icons
 
-ReactDOM.render(<Provider store={store}> <App ref={UserService.initKeycloak}/> </Provider>, document.getElementById('root'));
-
-HttpService.configure();
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
